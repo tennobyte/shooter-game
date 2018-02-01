46e4f1d987b032a5725e231a925dca29457a9095
tennobyte <slowpok.fake@gmail.com>
2018-02-02 00:45:54 +1000

    Main menu implementation

---
+++ Shooter-game/Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Slider loadingSlider;
+    public GameObject loadingScreen;
+    public GameObject mainMenu;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadLevelAsync(sceneIndex));
+    }
+
+    IEnumerator LoadLevelAsync(int sceneIndex)
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!loading.isDone)
+        {
+            float loadingProgress = Mathf.Clamp01(loading.progress / .9f);
+            loadingSlider.value = loadingProgress;
+            yield return null;
+        }
+    }
+
+}
---
+++ Shooter-game/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour {
+
+    private int levelIndex = 1;
+
+    public GameObject levelLoader;
+
+    public void PlayGame()
+    {
+        levelLoader.GetComponent<LevelLoader>().LoadLevel(levelIndex);
+        //StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
