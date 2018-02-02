9bad94fe9e5b8f538c2b983a8d4bff354ada2fa1
tennobyte <slowpok.fake@gmail.com>
2018-02-02 20:27:17 +1000

    Game basic interface

---
+++ Shooter-game/Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundMove : MonoBehaviour {
+    
+    public float layerOneSpeed;
+    public float layerTwoSpeed;
+    public float layerThreeSpeed;
+
+    public RawImage layerOne;
+    public RawImage layerTwo;
+    public RawImage layerThree;
+
+    // Update is called once per frame
+    void Update () {
+        //layerOne.uvRect.Set(layerOne.uvRect.x, layerOne.uvRect.y + Time.deltaTime*layerOneSpeed, 1,1);
+        //Vector2 offset = new Vector2(0, Time.time * layerOneSpeed);
+        layerOne.uvRect = new Rect(layerOne.uvRect.x, layerOne.uvRect.y + Time.deltaTime * layerOneSpeed, 1, 1);
+        layerTwo.uvRect = new Rect(layerTwo.uvRect.x, layerTwo.uvRect.y + Time.deltaTime * layerTwoSpeed, 1, 1);
+        layerThree.uvRect = new Rect(layerThree.uvRect.x, layerThree.uvRect.y + Time.deltaTime * layerThreeSpeed, 1, 1);
+        //layerOne.material.mainTextureOffset = offset;
+        //Debug.Log(Time.time);
+
+    }
+}
---
+++ Shooter-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject leftGun;
+    public GameObject rightGun;
+    public GameObject playerShip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
